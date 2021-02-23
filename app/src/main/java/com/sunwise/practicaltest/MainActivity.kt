a90d3bb3f9c3e955a139a4f7bb0a05bd4463c635
Jose Cardenas <jcardenasmon@outlook.com>
2021-02-23 16:50:24 -0500

    basic structure

---
+++ app/src/main/java/com/sunwise/practicaltest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sunwise.practicaltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
